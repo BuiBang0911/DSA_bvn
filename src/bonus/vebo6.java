30c04c58672771c56a61cd07e17e5dd836565a20
BuiBang0911 <bui09bang11@gmail.com>
2023-12-07 22:43:51 +0700

    Create vebo6.java

---
+++ src/bonus/vebo6.java
@@ -0,0 +1,69 @@
+package bonus;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'cookies' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY A
+     */
+
+    public static int cookies(int k, List<Integer> A) {
+        // Write your code here
+        Queue<Integer> pqueue = new PriorityQueue<>();
+        for (int i = 0; i < A.size(); i++) {
+            pqueue.add(A.get(i));
+        }
+        int cnt = 0;
+        while(pqueue.peek() < k && pqueue.size() > 1) {
+            int x = pqueue.poll();
+            pqueue.add(x + 2 * pqueue.poll());
+            cnt++;
+        }
+        if (pqueue.isEmpty() || pqueue.peek() < k) return -1;
+        return cnt;
+    }
+
+}
+
+public class vebo6 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] ATemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> A = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int AItem = Integer.parseInt(ATemp[i]);
+            A.add(AItem);
+        }
+
+        int result = Result.cookies(k, A);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
